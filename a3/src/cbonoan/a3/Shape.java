7dd2bcaaadbe222737e401d9588ec8b3d1dcad0f
Charles Bonoan <charlesbonoan@gmail.com>
2021-02-10 00:07:15 -0800

    Added getArea() function to Shape class in order to use in child classes

--- a3/src/cbonoan/a3/Shape.java
+++ a3/src/cbonoan/a3/Shape.java
@@ -9,6 +9,7 @@ import static java.lang.Math.PI;
 public class Shape {
     private Type type;
 
+    public double getArea() { return 0; } //Need this function in order to use getArea() function in child classes
     public Type getType() { return this.type; }
     public void setType(Type type) { this.type = type; }
     public double pi = PI;
